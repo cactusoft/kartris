cc5e554326404b389db46b15b90edf33469df24d
Kartris_cp <SND\Kartris_cp>
2013-03-21 18:19:50 +0000

    Hyperlinked file name, fixed issue with category menu disappearing

--- Admin/_GeneralFiles.aspx.vb
+++ Admin/_GeneralFiles.aspx.vb
@@ -22,6 +22,7 @@ Partial Class Admin_GeneralFiles
     Dim blnFileSaved As Boolean = False
 
     Dim strFilesFolder As String = KartSettingsManager.GetKartConfig("general.uploadfolder") & "General/"
+
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Page.IsPostBack Then
             Page.ClientScript.GetPostBackEventReference(Me, String.Empty)
@@ -32,37 +33,41 @@ Partial Class Admin_GeneralFiles
         Page.Title = GetGlobalResourceObject("_Kartris", "PageTitle_GeneralFiles") & " | " & GetGlobalResourceObject("_Kartris", "ContentText_KartrisName")
     End Sub
 
+    Public Function FormatFilePath(ByVal strFilename As String) As String
+        Return strFilesFolder & strFilename
+    End Function
+
     Private Sub LoadGeneralFiles()
-        Dim dtFiles As New DataTable
-        dtFiles.Columns.Add(New DataColumn("F_Name", Type.GetType("System.String")))
-        dtFiles.Columns.Add(New DataColumn("F_Type", Type.GetType("System.String")))
-        dtFiles.Columns.Add(New DataColumn("F_Size", Type.GetType("System.String")))
-        dtFiles.Columns.Add(New DataColumn("F_LastUpdated", Type.GetType("System.String")))
+        Dim dtbFiles As New DataTable
+        dtbFiles.Columns.Add(New DataColumn("F_Name", Type.GetType("System.String")))
+        dtbFiles.Columns.Add(New DataColumn("F_Type", Type.GetType("System.String")))
+        dtbFiles.Columns.Add(New DataColumn("F_Size", Type.GetType("System.String")))
+        dtbFiles.Columns.Add(New DataColumn("F_LastUpdated", Type.GetType("System.String")))
 
         Dim dirGeneralFiles As New DirectoryInfo(Server.MapPath(strFilesFolder))
-        Dim fInfo() As FileInfo
+        Dim filInfo() As FileInfo
         If Not dirGeneralFiles.Exists() Then Exit Sub
-        fInfo = dirGeneralFiles.GetFiles()
+        filInfo = dirGeneralFiles.GetFiles()
         Dim strName As String = "", strType As String = "", _
             strSize As String = "", strLastUpdated As String = ""
-        For i As Integer = 0 To fInfo.GetUpperBound(0)
-            strName = fInfo(i).Name
-            strType = fInfo(i).Extension
-            strSize = CStr(fInfo(i).Length() / 1000) & " KB"
-            strLastUpdated = CkartrisDisplayFunctions.FormatDate(fInfo(i).LastWriteTime, "t", Session("_LANG"))
+        For i As Integer = 0 To filInfo.GetUpperBound(0)
+            strName = filInfo(i).Name
+            strType = filInfo(i).Extension
+            strSize = CStr(filInfo(i).Length() / 1000) & " KB"
+            strLastUpdated = CkartrisDisplayFunctions.FormatDate(filInfo(i).LastWriteTime, "t", Session("_LANG"))
             If strName.ToLower = "web.config" Then Continue For
-            dtFiles.Rows.Add(strName, strType, strSize, strLastUpdated)
+            dtbFiles.Rows.Add(strName, strType, strSize, strLastUpdated)
         Next
 
-        If dtFiles.Rows.Count > 0 Then
-            gvwFiles.DataSource = dtFiles
+        If dtbFiles.Rows.Count > 0 Then
+            gvwFiles.DataSource = dtbFiles
             gvwFiles.DataBind()
             mvwGeneralFiles.SetActiveView(viwFiles)
         Else
             mvwGeneralFiles.SetActiveView(viwNoFiles)
         End If
-        updFiles.Update()
     End Sub
+
     Protected Sub gvwFiles_PageIndexChanging(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.GridViewPageEventArgs) Handles gvwFiles.PageIndexChanging
         gvwFiles.PageIndex = e.NewPageIndex
         LoadGeneralFiles()
@@ -91,14 +96,16 @@ Partial Class Admin_GeneralFiles
             litStatus2.Text = GetGlobalResourceObject("_Kartris", "ContentText_NoFile")
             popExtender2.Show()
         End If
+        CType(Me.Master, Skins_Admin_Template).LoadCategoryMenu()
+        updMain.Update()
     End Sub
 
     Function IsFileExist(strFileName As String) As Boolean
         Dim dirGeneralFiles As New DirectoryInfo(Server.MapPath(strFilesFolder))
         If Not dirGeneralFiles.Exists() Then Directory.CreateDirectory(Server.MapPath(strFilesFolder))
-        Dim fInfo() As FileInfo
-        fInfo = dirGeneralFiles.GetFiles(strFileName)
-        If fInfo.Length > 0 Then Return True
+        Dim filInfo() As FileInfo
+        filInfo = dirGeneralFiles.GetFiles(strFileName)
+        If filInfo.Length > 0 Then Return True
         Return False
     End Function
 
@@ -114,13 +121,15 @@ Partial Class Admin_GeneralFiles
             _UC_PopupMsg.ShowConfirmation(MESSAGE_TYPE.Confirmation, strMessage)
         End If
     End Sub
+
     Protected Sub _UC_PopupMsg_Confirmed() Handles _UC_PopupMsg.Confirmed
         If Not String.IsNullOrEmpty(hidFileNameToDelete.Value) Then
             Try
                 File.Delete(Server.MapPath(strFilesFolder & hidFileNameToDelete.Value))
                 hidFileNameToDelete.Value = ""
-                CType(Me.Master, Skins_Admin_Template).DataUpdated()
                 LoadGeneralFiles()
+                CType(Me.Master, Skins_Admin_Template).DataUpdated()
+                updMain.Update()
             Catch ex As Exception
                 _UC_PopupMsg.ShowConfirmation(MESSAGE_TYPE.ErrorMessage, ex.Message)
             End Try
