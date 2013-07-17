4fc038782148f770da4f9137db29cb94c6537089
cactusoft <cactusoft@users.noreply.github.com>
2013-07-17 19:49:57 +0000

    Updated version number and release date

--- App_Code/Kartris.vb
+++ App_Code/Kartris.vb
@@ -29,8 +29,8 @@ Imports System.Xml
 ''' </summary>
 Public NotInheritable Class CkartrisEnumerations
 
-    Public Const KARTRIS_VERSION As Decimal = 2.5
-    Public Const KARTRIS_VERSION_ISSUE_DATE As Date = #6/9/2013# '' MM/dd/yyyy 
+    Public Const KARTRIS_VERSION As Decimal = 2.5001
+    Public Const KARTRIS_VERSION_ISSUE_DATE As Date = #7/17/2013# '' MM/dd/yyyy 
 
     Public Enum LANG_ELEM_TABLE_TYPE
         Versions = 1
