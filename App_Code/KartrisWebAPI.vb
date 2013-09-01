b0fd8a0526dbfcb7342c8aaa52b0d0a0915dacd5
knightmedz_cp <SND\knightmedz_cp>
2013-09-01 05:47:31 +0000

    Added authentication code to the Web API. Now supports methods that have object class parameters

--- App_Code/KartrisWebAPI.vb
+++ App_Code/KartrisWebAPI.vb
@@ -15,8 +15,10 @@
 
 Imports System.Reflection
 Imports Kartris
+Imports System.ServiceModel.Activation
 
-Public Class KartrisWebAPI
+<AspNetCompatibilityRequirements(RequirementsMode:=AspNetCompatibilityRequirementsMode.Required)> _
+ Public Class KartrisWebAPI
     Implements IKartrisWebAPI
 
     ''' <summary>
@@ -28,108 +30,189 @@ Public Class KartrisWebAPI
     ''' <remarks></remarks>
     Public Function Execute(ByVal strMethod As String, ByVal strParametersXML As String) As String Implements IKartrisWebAPI.Execute
         Dim strOutput As String = ""
-        Try
-            Dim obj As New Object
+        Dim blnKeyValidated As Boolean = False
+        Dim blnAllowIP As Boolean = True
 
-            Dim strClassName As String = Left(strMethod, InStr(strMethod, ".") - 1)
-            Dim strMethodName As String = Mid(strMethod, InStr(strMethod, ".") + 1)
+        Dim strAuthorizationKey As String = HttpContext.Current.Request.Headers("Authorization")
+        blnKeyValidated = ValidateKey(strAuthorizationKey)
 
-            Dim t As Type = System.Type.GetType(strClassName)
+        If Not blnKeyValidated Then strOutput = "Sorry. Cant authenticate request!"
 
-            Dim m As MethodInfo
+        'Get user's IP address
+        Dim strClientIP As String = HttpContext.Current.Request.ServerVariables("HTTP_X_FORWARDED_FOR")
+        If String.IsNullOrEmpty(strClientIP) Then
+            strClientIP = HttpContext.Current.Request.ServerVariables("REMOTE_ADDR")
+        End If
 
-            Dim Params() As Object = Nothing
-
-            If t IsNot Nothing Then
-                'method was found, try to load it up
-                If strParametersXML Is Nothing OrElse String.IsNullOrEmpty(strParametersXML) Then
-                    m = t.GetMethod(strMethodName, Type.EmptyTypes)
-                Else
-                    '<?xml version="1.0" encoding="utf-8" ?>
-                    '<KartrisWebAPI>
-                    '  <Parameters>
-                    '    <Parameter Name="Param1" Type="String" Value="Value1">
-                    '    <Parameter Name="Param2" Type="Integer" Value="Value2">
-                    '    ...
-                    '  </Parameters>
-                    '<KartrisWebAPI>
-
-                    'parse the parameter array XML
-                    Dim docXML As XmlDocument = New XmlDocument
-                    'Load the xml 
-                    docXML.LoadXml(strParametersXML)
-
-                    Dim lstNodes As XmlNodeList
-                    Dim ndeParameter As XmlNode
-
-                    Dim strParametersNodePath As String = "/KartrisWebAPI/Parameters/Parameter"
-                    lstNodes = docXML.SelectNodes(strParametersNodePath)
-
-                    Array.Resize(Params, lstNodes.Count)
-
-                    Dim intIndex As Integer = 0
-                    For Each ndeParameter In lstNodes
-
-                        Dim objParam As New Object
-                        Dim strType As String = ndeParameter.Attributes.GetNamedItem("Type").Value
-                        Dim strValue As String = ndeParameter.Attributes.GetNamedItem("Value").Value
-
-                        'cast the generic object variable just to be safe
-                        Select Case strType.ToLower
-                            Case "string"
-                                objParam = strValue
-                            Case "integer"
-                                objParam = CInt(strValue)
-                            Case "boolean"
-                                If strValue.ToLower = "true" Then objParam = True Else objParam = False
-                            Case "address"
-                                ' TODO: add support to Address type parameters in Web API
-                                objParam = DirectCast(objParam, KartrisClasses.Address)
-                            Case "long"
-                                objParam = CLng(strValue)
-                            Case "short"
-                                objParam = CShort(strValue)
-                            Case "arraylist"
-                                ' TODO: add support to arraylist type parameters in Web API
-                                objParam = DirectCast(objParam, System.Collections.ArrayList)
-                            Case "double"
-                                objParam = CDbl(strValue)
-                            Case "byte"
-                                objParam = CByte(strValue)
-                            Case "single"
-                                objParam = CSng(strValue)
-                        End Select
-
-                        Params(intIndex) = objParam
-                        intIndex = intIndex + 1
-                    Next
-
-                    Dim pTypes(Params.Length - 1) As Type
-
-                    For i As Integer = 0 To Params.GetUpperBound(0)
-                        pTypes(i) = Params(i).GetType()
-                    Next
-
-                    m = t.GetMethod(strMethodName, pTypes)
+        'Check matches specified IPs in web.config, if not blank
+        Dim strBackEndIPLock = ConfigurationManager.AppSettings("KartrisWebAPIIPLock").ToString()
+        If strBackEndIPLock <> "" Then
+            Try
+                Dim arrBackendIPs As String() = Split(strBackEndIPLock, ",")
+                Dim blnFullIP As Boolean
+                For x As Integer = 0 To arrBackendIPs.Count - 1
+                    'check if the IP is a range or a full IP, if its a full ip then it must be matched exactly
+                    If Split(arrBackendIPs(x), ".").Count = 4 Then blnFullIP = True Else blnFullIP = False
+                    If IIf(blnFullIP, arrBackendIPs(x) = strClientIP, Left(strClientIP, arrBackendIPs(x).Length) = arrBackendIPs(x)) Then
+                        'ok, let 'em in
+                        blnAllowIP = True
+                        Exit For
+                    End If
+                Next
+                If Not blnAllowIP Then
+                    strOutput = "Sorry. Cant authenticate request!"
                 End If
+            Catch ex As Exception
+                blnAllowIP = False
+                strOutput = "Invalid Web API IP lock settings!"
+            End Try
+        End If
 
-                If m IsNot Nothing Then
-                    strOutput = Payment.Serialize(m.Invoke(obj, Params))
+        'HttpContext.Current.Request.UserHostAddress
+        If blnKeyValidated And blnAllowIP Then
+            Try
+                Dim obj As New Object
+
+                Dim strClassName As String = Left(strMethod, InStr(strMethod, ".") - 1)
+                Dim strMethodName As String = Mid(strMethod, InStr(strMethod, ".") + 1)
+
+                Dim t As Type = System.Type.GetType(strClassName)
+
+                Dim m As MethodInfo
+
+                Dim Params() As Object = Nothing
+
+                If t IsNot Nothing Then
+                    'method was found, try to load it up
+                    If strParametersXML Is Nothing OrElse String.IsNullOrEmpty(strParametersXML) Then
+                        m = t.GetMethod(strMethodName, Type.EmptyTypes)
+                    Else
+                        '<?xml version="1.0" encoding="utf-8" ?>
+                        '<KartrisWebAPI>
+                        '  <Parameters>
+                        '    <Parameter Name="Param1" Type="String">
+                        '       <Value>Value1</Value>
+                        '    </Parameter>
+                        '    <Parameter Name="Param2" Type="Integer">
+                        '       <Value>Value2</Value>
+                        '    </Parameter>
+                        '    ...
+                        '  </Parameters>
+                        '<KartrisWebAPI>
+
+                        'parse the parameter array XML
+                        Dim docXML As XmlDocument = New XmlDocument
+                        'Load the xml 
+                        docXML.LoadXml(strParametersXML)
+
+                        Dim lstNodes As XmlNodeList
+                        Dim ndeParameter As XmlNode
+
+                        Dim strParametersNodePath As String = "/KartrisWebAPI/Parameters/Parameter"
+                        lstNodes = docXML.SelectNodes(strParametersNodePath)
+
+                        Array.Resize(Params, lstNodes.Count)
+
+                        Dim intIndex As Integer = 0
+                        For Each ndeParameter In lstNodes
+
+                            Dim objParam As New Object
+                            Dim strType As String = ndeParameter.Attributes.GetNamedItem("Type").Value
+                            Dim strValue As String = ndeParameter.FirstChild.InnerText
+
+                            Select Case strType.ToLower
+                                Case "string"
+                                    objParam = strValue
+                                Case "integer"
+                                    objParam = CInt(strValue)
+                                Case "boolean"
+                                    If strValue.ToLower = "true" Then objParam = True Else objParam = False
+                                Case "address"
+                                    Dim objAddress As KartrisClasses.Address = Payment.Deserialize(strValue, GetType(KartrisClasses.Address))
+                                    objParam = objAddress
+                                Case "long"
+                                    objParam = CLng(strValue)
+                                Case "short"
+                                    objParam = CShort(strValue)
+                                Case "arraylist"
+                                    Dim arrObject As ArrayList = Payment.Deserialize(strValue, GetType(ArrayList))
+                                    objParam = arrObject
+                                Case "basket"
+                                    Dim objBasket As BasketBLL = Payment.Deserialize(strValue, GetType(BasketBLL))
+                                    objParam = objBasket
+                                Case "datatable"
+                                    Dim tblObject As DataTable = Payment.Deserialize(strValue, GetType(DataTable))
+                                    objParam = tblObject
+                                Case "double"
+                                    objParam = CDbl(strValue)
+                                Case "byte"
+                                    objParam = CByte(strValue)
+                                Case "single"
+                                    objParam = CSng(strValue)
+                            End Select
+
+                            Params(intIndex) = objParam
+                            intIndex = intIndex + 1
+                        Next
+
+                        Dim pTypes(Params.Length - 1) As Type
+
+                        For i As Integer = 0 To Params.GetUpperBound(0)
+                            pTypes(i) = Params(i).GetType()
+                        Next
+
+                        m = t.GetMethod(strMethodName, pTypes)
+                    End If
+
+                    If m IsNot Nothing Then
+                        strOutput = Payment.Serialize(m.Invoke(obj, Params))
+                    Else
+                        Throw New Exception("Can't find method """ & strMethodName & """ in Class """ & strClassName & """")
+                    End If
+
                 Else
-                    Throw New Exception("Can't find method """ & strMethodName & """ in Class """ & strClassName & """")
+                    'method wasn't found
+                    strOutput = Payment.Serialize("Can't find specified Class """ & strClassName & """")
                 End If
-
-            Else
-                'method wasn't found
-                strOutput = Payment.Serialize("Can't find specified Class """ & strClassName & """")
-            End If
-        Catch ex As Exception
-            'an exception occured while try to parse strMethod
-            strOutput = Payment.Serialize("Error parsing method string! Exception Details -> " & ex.Message)
-        End Try
+            Catch ex As Exception
+                'an exception occured while try to parse strMethod
+                strOutput = Payment.Serialize("Error parsing method string! Exception Details -> " & ex.Message)
+            End Try
+        End If
 
         Return strOutput
 
     End Function
+    ''' <summary>
+    ''' Validate the key in the request authorization header against the KartrisWebAPISecretKey appsetting
+    ''' </summary>
+    ''' <param name="strKartrisAPIKey"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function ValidateKey(ByVal strKartrisAPIKey As String) As Boolean
+        Dim strConfigAPIKey As String = ConfigurationManager.AppSettings("KartrisWebAPISecretKey")
+        'always invalidate request if API key is not set in the web.config (its safer that way)
+        If String.IsNullOrEmpty(strConfigAPIKey) Then Return False
+        If (strKartrisAPIKey = strConfigAPIKey) Then Return True Else Return False
+    End Function
 
+End Class
+Public Class UserNamePassValidator
+    Inherits System.IdentityModel.Selectors.UserNamePasswordValidator
+    ''' <summary>
+    ''' This method is only used when wsHTTPBinding is enabled
+    ''' Uses backend login details to authenticate API user in addition to the WebAPISecretKey validation
+    ''' </summary>
+    ''' <param name="userName"></param>
+    ''' <param name="password"></param>
+    ''' <remarks></remarks>
+    Public Overrides Sub Validate(userName As String, password As String)
+        If userName Is Nothing OrElse password Is Nothing Then
+            Throw New ArgumentNullException()
+        End If
+
+        If Not LoginsBLL.Validate(userName, password) Then
+            Throw New System.ServiceModel.FaultException("Incorrect Username or Password")
+        End If
+    End Sub
 End Class
