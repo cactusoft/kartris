6d67187ba6c6619a96b930a285008d9223700a5f
MohammadAwad_cp <SND\MohammadAwad_cp>
2013-10-10 21:45:10 +0000

    Optimizing the _spKartrisDB_GetTaskList

---
+++ Uploads/Resources/UpdateSQL_2.5005_To_2.5006.sql
@@ -0,0 +1,41 @@
+﻿/****** Object:  StoredProcedure [dbo].[_spKartrisDB_GetTaskList]    Script Date: 01/23/2013 21:59:09 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Mohammad
+-- Modified by:		Medz 02/11/09
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER PROCEDURE [dbo].[_spKartrisDB_GetTaskList]
+(	
+	@NoOrdersToInvoice as int OUTPUT,
+	@NoOrdersNeedPayment as int OUTPUT,
+	@NoOrdersToDispatch as int OUTPUT,
+	@NoStockWarnings as int OUTPUT,
+	@NoOutOfStock as int OUTPUT,
+	@NoReviewsWaiting as int OUTPUT,
+	@NoAffiliatesWaiting as int OUTPUT,
+	@NoCustomersWaitingRefunds as int OUTPUT,
+	@NoCustomersInArrears as int OUTPUT,
+	@NoOfLinnworksOrdersToBeSent as int OUTPUT
+)
+AS
+BEGIN
+	SELECT @NoOrdersToInvoice = Count(O_ID) FROM dbo.tblKartrisOrders WHERE O_Invoiced = 'False' AND O_Paid = 'False' AND O_Sent = 'True' AND O_Cancelled = 'False';
+	SELECT @NoOrdersNeedPayment = Count(O_ID) FROM dbo.tblKartrisOrders WHERE O_Paid = 'False' AND O_Invoiced = 'True' AND O_Sent = 'True' AND O_Cancelled = 'False';
+	SELECT @NoOrdersToDispatch = Count(O_ID) FROM dbo.tblKartrisOrders WHERE O_Sent = 'True' AND O_Paid = 'True' AND O_Shipped = 'False' AND O_Cancelled = 'False';
+	
+	SELECT @NoStockWarnings = Count(V_ID) FROM dbo.tblKartrisVersions WHERE V_QuantityWarnLevel >= V_Quantity AND V_QuantityWarnLevel <> 0;
+	SELECT @NoOutOfStock = Count(V_ID) FROM dbo.tblKartrisVersions WHERE V_Quantity = 0 AND V_QuantityWarnLevel <> 0;
+
+	SELECT @NoReviewsWaiting = Count(REV_ID) FROM dbo.tblKartrisReviews WHERE REV_Live = 'a';
+	SELECT @NoAffiliatesWaiting  = Count(U_ID) FROM dbo.tblKartrisUsers WHERE U_IsAffiliate = 'True' AND U_AffiliateCommission = 0;
+	SELECT @NoCustomersWaitingRefunds  = Count(U_ID) FROM dbo.tblKartrisUsers WHERE U_CustomerBalance > 0;
+	SELECT @NoCustomersInArrears  = Count(U_ID) FROM dbo.tblKartrisUsers WHERE U_CustomerBalance < 0;
+
+	SELECT @NoOfLinnworksOrdersToBeSent = Count(O_ID) FROM tblKartrisOrders WHERE (O_Paid = 1) AND (O_Cancelled IS NULL or O_Cancelled = 0) AND O_ID NOT IN (SELECT [LWO_OrderID] FROM [dbo].[tblKartrisLinnworksOrders])
+END
+GO
