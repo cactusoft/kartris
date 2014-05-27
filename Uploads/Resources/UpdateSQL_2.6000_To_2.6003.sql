3b1735a4e42f68ff09132be11e9a2615316e4960
cactusoft <cactusoft@users.noreply.github.com>
2014-05-27 12:28:56 +0000

    Updated split string function to 500 chars otherwise problems with attribute filtering in powerpack

---
+++ Uploads/Resources/UpdateSQL_2.6000_To_2.6003.sql
@@ -0,0 +1,32 @@
+﻿/****** Object:  UserDefinedFunction [dbo].[fnTbl_SplitStrings]    Script Date: 01/23/2013 21:59:11 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Mohammad
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+ALTER FUNCTION [dbo].[fnTbl_SplitStrings]
+(	
+	@List varchar(max))
+RETURNS @ParsedList 
+table(_ID varchar(500)) AS BEGIN
+	DECLARE @_ID varchar(500), @Pos int    
+	SET @List = LTRIM(RTRIM(@List))+ ','    
+	SET @Pos = CHARINDEX(',', @List, 1)    
+	IF REPLACE(@List, ',', '') <> '' BEGIN        
+		WHILE @Pos > 0 BEGIN                
+			SET @_ID = LTRIM(RTRIM(LEFT(@List, @Pos - 1)))                
+			IF @_ID <> '' BEGIN                        
+				INSERT INTO @ParsedList (_ID)                         
+				VALUES (@_ID)              
+			END                
+			SET @List = RIGHT(@List, LEN(@List) - @Pos)                
+			SET @Pos = CHARINDEX(',', @List, 1)        
+		END    
+	END     
+	RETURN
+END
+GO
