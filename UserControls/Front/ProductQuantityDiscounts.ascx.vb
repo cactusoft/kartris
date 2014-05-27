e73f1e9dedb2c68bb3325da36960c639c6e0dd10
Kartris_cp <SND\Kartris_cp>
2014-05-27 13:55:48 +0000

    Override the display value for quantity discounts with the customer group price if it is lower (as this is what customer will pay)

--- UserControls/Front/ProductQuantityDiscounts.ascx.vb
+++ UserControls/Front/ProductQuantityDiscounts.ascx.vb
@@ -52,18 +52,19 @@ Partial Class ProductQuantityDiscounts
     End Sub
 
     Protected Sub CheckForQuantityDiscount()
-        Dim tblQuanityDiscount As DataTable = VersionsBLL.GetQuantityDiscountByProduct(ProductID, Session("LANG"))
-        If tblQuanityDiscount.Rows.Count > 0 Then
+        Dim tblQuantityDiscount As DataTable = VersionsBLL.GetQuantityDiscountByProduct(ProductID, Session("LANG"))
+        If tblQuantityDiscount.Rows.Count > 0 Then
             Dim tblVersions As New DataTable
             tblVersions.Columns.Add(New DataColumn("VersionName", Type.GetType("System.String")))
             tblVersions.Columns.Add(New DataColumn("VersionCode", Type.GetType("System.String")))
+            tblVersions.Columns.Add(New DataColumn("VersionID", Type.GetType("System.Int32")))
             Dim blnVersionExist As Boolean
-            For Each drwDiscount As DataRow In tblQuanityDiscount.Rows
+            For Each drwDiscount As DataRow In tblQuantityDiscount.Rows
                 blnVersionExist = False
                 For Each v_row As DataRow In tblVersions.Rows
                     If v_row("VersionName") = drwDiscount("V_Name") Then blnVersionExist = True : Exit For
                 Next
-                If Not blnVersionExist Then tblVersions.Rows.Add(drwDiscount("V_Name"), drwDiscount("V_CodeNumber"))
+                If Not blnVersionExist Then tblVersions.Rows.Add(drwDiscount("V_Name"), drwDiscount("V_CodeNumber"), drwDiscount("V_ID"))
             Next
             rptQuantityDiscount.DataSource = tblVersions
             rptQuantityDiscount.DataBind()
@@ -78,15 +79,46 @@ Partial Class ProductQuantityDiscounts
     Protected Sub rptQuantityDiscount_ItemDataBound(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.RepeaterItemEventArgs) Handles rptQuantityDiscount.ItemDataBound
         If e.Item.ItemType = ListItemType.AlternatingItem OrElse _
             e.Item.ItemType = ListItemType.Item Then
-            Using tblQuanityDiscount As DataTable = VersionsBLL.GetQuantityDiscountByProduct(ProductID, Session("LANG"))
+
+            'We need customer ID because we want to hide quantity
+            'discounts where price is higher than the customer group
+            'pricing (and hence the lower customer group price will
+            'be what is actually charged)
+            Dim numCustomerID As Integer = 0
+            If Context.User.Identity.IsAuthenticated Then
+                Try
+                    numCustomerID = DirectCast(Page, PageBaseClass).CurrentLoggedUser.ID
+                Catch ex As Exception
+                    FormsAuthentication.SignOut()
+                    numCustomerID = 0
+                End Try
+            Else
+                numCustomerID = 0
+            End If
+
+            'What version is this?
+            Dim numVersionID As Integer = CType(e.Item.FindControl("hidVersionID"), HiddenField).Value
+
+            'Get the customer group price for this version
+            Dim objBasket As New BasketBLL
+            Dim numCustomerGroupPrice As Double = objBasket.GetCustomerGroupPriceForVersion(numCustomerID, numVersionID)
+
+            Using tblQuantityDiscount As DataTable = VersionsBLL.GetQuantityDiscountByProduct(ProductID, Session("LANG"))
                 Dim strVersionCode As String = CType(e.Item.FindControl("litVersionCode_Hidden"), Literal).Text
-                Dim drwDiscount() As DataRow = tblQuanityDiscount.Select("V_CodeNumber='" & strVersionCode & "'")
+
+                Dim drwDiscount() As DataRow = tblQuantityDiscount.Select("V_CodeNumber='" & strVersionCode & "'")
                 Using tblDiscounts As New DataTable
                     tblDiscounts.Columns.Add(New DataColumn("Quantity", Type.GetType("System.String")))
                     tblDiscounts.Columns.Add(New DataColumn("Price", Type.GetType("System.String")))
                     For i As Integer = 0 To drwDiscount.Length - 1
                         Dim numPrice As Single = CurrenciesBLL.ConvertCurrency(Session("CUR_ID"), CDbl(FixNullFromDB(drwDiscount(i)("QD_Price"))))
 
+                        'If the numPrice from quantity discounts is higher than the 
+                        'customer group price, we use the lower customer group price
+                        'instead
+                        If numPrice > numCustomerGroupPrice Then numPrice = numCustomerGroupPrice
+
+                        'Format and add the price
                         tblDiscounts.Rows.Add(CStr(drwDiscount(i)("QD_Quantity")) & "+", _
                                 CurrenciesBLL.FormatCurrencyPrice(Session("CUR_ID"), numPrice))
                     Next
