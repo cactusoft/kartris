81e9c1a221dfe89fa6d034c42f5c7bf96e9dbc08
Kartris_cp <SND\Kartris_cp>
2014-08-26 15:56:20 +0000

    Updated general.kartrisinfo.versionadded to 2.7 to identify correct database version to data tool

--- Uploads/Resources/UpdateSQL_2.6005_To_2.7000.sql
+++ Uploads/Resources/UpdateSQL_2.6005_To_2.7000.sql
@@ -64,3 +64,5 @@ table(_ID varchar(500)) AS BEGIN
 	RETURN
 END
 GO
+/****** set this to tell Data tool which version of db we have ******/
+UPDATE tblKartrisConfig SET CFG_Value='2.7000', CFG_VersionAdded=2.7 WHERE CFG_Name='general.kartrisinfo.versionadded';
