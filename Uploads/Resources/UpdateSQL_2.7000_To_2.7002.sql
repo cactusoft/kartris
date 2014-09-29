a5baf52011995cacfd80ad38a287729cc5785da1
cactusoft <cactusoft@users.noreply.github.com>
2014-09-29 11:30:12 +0000

    Updated attribute sort order

---
+++ Uploads/Resources/UpdateSQL_2.7000_To_2.7002.sql
@@ -0,0 +1,28 @@
+﻿
+
+-- =============================================
+-- Author:		Mohammad
+-- Description:	Updated by Paul, change field
+-- in ORDER BY clause
+-- =============================================
+ALTER PROCEDURE [dbo].[spKartrisAttributes_GetSummaryByProductID]
+	(
+		@P_ID int,
+		@LANG_ID tinyint
+	)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+ SELECT     vKartrisTypeAttributes.ATTRIB_ID, vKartrisTypeAttributes.ATTRIB_Name, vKartrisTypeAttributeValues.ATTRIBV_Value,
+			vKartrisTypeAttributeValues.ATTRIBV_ProductID, vKartrisTypeAttributes.ATTRIB_Compare
+FROM         vKartrisTypeAttributeValues INNER JOIN
+					  vKartrisTypeAttributes ON vKartrisTypeAttributeValues.ATTRIBV_AttributeID = vKartrisTypeAttributes.ATTRIB_ID AND 
+					  vKartrisTypeAttributeValues.LANG_ID = vKartrisTypeAttributes.LANG_ID
+WHERE     (vKartrisTypeAttributes.ATTRIB_Live = 1) AND (vKartrisTypeAttributes.ATTRIB_ShowFrontend = 1) AND (vKartrisTypeAttributeValues.ATTRIBV_ProductID = @P_ID) AND
+					   (vKartrisTypeAttributeValues.LANG_ID = @LANG_ID)
+ORDER BY vKartrisTypeAttributes.ATTRIB_OrderByValue
+END
+GO
