8daadb70aabd07fd6c71e78d2c581ac9811f0591
cactusoft <cactusoft@users.noreply.github.com>
2015-02-27 01:22:21 +0000

    HugoFox Skin System added

---
+++ Skins/Hugofox/Database/HugoFox_Database_Update.sql
@@ -0,0 +1,4 @@
+﻿/****** HUGOFOX DATABASE UPDATE ******/
+INSERT [dbo].[tblKartrisConfig] ([CFG_Name], [CFG_Value], [CFG_DataType], [CFG_DisplayType], [CFG_DisplayInfo], [CFG_Description], [CFG_VersionAdded], [CFG_DefaultValue], [CFG_Important]) VALUES (N'hugofox.account.id', N'0', N'n', N't', N'', N'Your HugoFox account ID number', 2.8003, N'0', 0)
+
+
