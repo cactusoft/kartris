6b610e7bae7ad686d7aa6b0d05563476409d14cc
Kartris_cp <SND\Kartris_cp>
2015-05-19 09:31:23 +0000

    Fixed attribute compare bug (smallint P_ID), GPWebPay typo fixed, product.asp handles forwarding CactuShop URLs to Kartris for upgrades

---
+++ Uploads/Resources/UpdateSQL_2.8004_To_2.8005.sql
@@ -0,0 +1,35 @@
+﻿/****** 
+v2.8005 
+Fixes spKartrisProducts_GetAttributeValue where P_ID was set as
+a smallint, meaning that on the comparison table, products with
+ID of more than 32,767 showed no attributes. Products DAL also
+updated.
+******/
+
+/****** Object:  StoredProcedure [dbo].[spKartrisProducts_GetAttributeValue]    Script Date: 2015-05-16 13:06:05 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Mohammad
+-- Create date: 
+-- Description:	
+-- =============================================
+ALTER PROCEDURE [dbo].[spKartrisProducts_GetAttributeValue]
+	(
+	@P_ID int,
+	@ATTRIB_ID int,
+	@LANG_ID tinyint,
+	@ATTRIBV_Value nvarchar(50) OUT
+	)
+AS
+SELECT @ATTRIBV_Value = ATTRIBV_Value 
+FROM vKartrisTypeAttributeValues 
+WHERE vKartrisTypeAttributeValues.ATTRIBV_AttributeID = @ATTRIB_ID
+	AND vKartrisTypeAttributeValues.ATTRIBV_ProductID = @P_ID
+	AND vKartrisTypeAttributeValues.LANG_ID = @LANG_ID
+
+/****** Set this to tell Data tool which version of db we have ******/
+UPDATE tblKartrisConfig SET CFG_Value='2.8005', CFG_VersionAdded=2.8005 WHERE CFG_Name='general.kartrisinfo.versionadded';
+GO
---
+++ product.asp
@@ -0,0 +1,26 @@
+﻿<%@ Language=VBScript %>
+<%
+'========================================================================
+'Kartris - www.kartris.com
+'Copyright 2015 CACTUSOFT
+
+'GNU GENERAL PUBLIC LICENSE v2
+'This program is free software distributed under the GPL without any
+'warranty.
+'www.gnu.org/licenses/gpl-2.0.html
+
+'KARTRIS COMMERCIAL LICENSE
+'If a valid license.config issued by Cactusoft is present, the KCL
+'overrides the GPL v2.
+'www.kartris.com/t-Kartris-Commercial-License.aspx
+'========================================================================
+
+'This is just a simple script to redirect product page links for 
+'upgraded CactuShop sites to the new Kartris product page URL.
+'Note that friendly style CactuShop links are handled in 04.aspx.vb
+'instead
+
+strID = request.querystring("P_ID")
+Response.Status="301 Moved Permanently"
+Response.AddHeader "Location","Product.aspx?ProductID=" & strID
+%>
