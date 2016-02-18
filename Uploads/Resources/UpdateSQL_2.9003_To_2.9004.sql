1f6aa56c88e021f6d721e4f60747593028aacd75
jonisilva_cp <SND\jonisilva_cp>
2016-02-18 17:55:24 +0000

    Update Script add

---
+++ Uploads/Resources/UpdateSQL_2.9003_To_2.9004.sql
@@ -0,0 +1,3 @@
+﻿--Insert Language Strings - Jóni Silva - 18/02/2016 BEGIN
+INSERT [dbo].[tblKartrisLanguageStrings] ([LS_FrontBack], [LS_Name], [LS_Value], [LS_Description], [LS_VersionAdded], [LS_DefaultValue], [LS_VirtualPath], [LS_ClassName], [LS_LangID]) VALUES (N'b', N'ContentText_InvoiceHeader', N'', NULL, 2.9004, N'', NULL, N'Invoice',1)
+INSERT [dbo].[tblKartrisLanguageStrings] ([LS_FrontBack], [LS_Name], [LS_Value], [LS_Description], [LS_VersionAdded], [LS_DefaultValue], [LS_VirtualPath], [LS_ClassName], [LS_LangID]) VALUES (N'b', N'ContentText_InvoiceFooter', N'', NULL, 2.9004, N'', NULL, N'Invoice',1)
