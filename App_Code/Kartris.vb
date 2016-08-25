ba7a3b32dc914beeeb775aa18a57304e3b740ac6
Kartris_cp <SND\Kartris_cp>
2016-08-25 09:55:44 +0000

    update issue date to 25.08.2016

--- App_Code/Kartris.vb
+++ App_Code/Kartris.vb
@@ -31,7 +31,7 @@ Imports System.Xml
 Public NotInheritable Class CkartrisEnumerations
 
     Public Const KARTRIS_VERSION As Decimal = 2.9006
-    Public Const KARTRIS_VERSION_ISSUE_DATE As Date = #7/25/2016# '' MM/dd/yyyy 
+    Public Const KARTRIS_VERSION_ISSUE_DATE As Date = #8/25/2016# '' MM/dd/yyyy 
 
     Public Enum LANG_ELEM_TABLE_TYPE
         Versions = 1
