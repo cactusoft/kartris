d4e8c0f8aab8989a57a170212f4834a063b6fb9a
Paul <paul@cactusoft.com>
2017-05-18 11:26:38 +0100

    Missing copyright info

--- UserControls/Front/RichSnippets.ascx.vb
+++ UserControls/Front/RichSnippets.ascx.vb
@@ -1,4 +1,19 @@
-﻿Imports CkartrisDataManipulation
+﻿'========================================================================
+'Kartris - www.kartris.com
+'Copyright 2017 CACTUSOFT
+
+'GNU GENERAL PUBLIC LICENSE v2
+'This program is free software distributed under the GPL without any
+'warranty.
+'www.gnu.org/licenses/gpl-2.0.html
+
+'KARTRIS COMMERCIAL LICENSE
+'If a valid license.config issued by Cactusoft is present, the KCL
+'overrides the GPL v2.
+'www.kartris.com/t-Kartris-Commercial-License.aspx
+'========================================================================
+Imports CkartrisDataManipulation
+
 Partial Class UserControls_Front_RichSnippets
     Inherits System.Web.UI.UserControl
 
