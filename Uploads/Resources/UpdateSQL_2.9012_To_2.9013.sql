e6e3b60ddf13c4f071e3904b5dedd336506bda51
Paul <paul@cactusoft.com>
2018-03-28 16:09:09 +0100

    Added setting, a BCC email address that mailing list signups can be sent to as proof for GDPR opt ins (proves what text was opted into)

---
+++ Uploads/Resources/UpdateSQL_2.9012_To_2.9013.sql
@@ -0,0 +1,9 @@
+﻿
+
+/****** new GDPR functionality ******/
+INSERT INTO [tblKartrisConfig]
+(CFG_Name,CFG_Value,CFG_DataType,CFG_DisplayType,CFG_DisplayInfo,CFG_Description,CFG_VersionAdded,CFG_DefaultValue,CFG_Important)
+VALUES
+(N'general.gdpr.mailinglistbcc', N'y', N's', N't',	'',N'An email address to BCC mailing list confirmations to as proof of GDPR terms',2.9013, N'y', 0);
+
+GO
