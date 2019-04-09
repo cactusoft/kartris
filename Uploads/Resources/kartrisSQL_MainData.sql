06d77f5e848049384b9adbda84aa3d611f4abbec
Paul <paul@cactusoft.com>
2019-04-09 10:31:27 +0100

    sql and other minor changes

--- Uploads/Resources/kartrisSQL_MainData.sql
+++ Uploads/Resources/kartrisSQL_MainData.sql
@@ -16201,144 +16201,7 @@ FROM         dbo.tblKartrisLanguageElements INNER JOIN
 					  dbo.tblKartrisPromotions ON dbo.tblKartrisLanguageElements.LE_ParentID = dbo.tblKartrisPromotions.PROM_ID
 WHERE     (dbo.tblKartrisLanguageElements.LE_TypeID = 7) AND (dbo.tblKartrisLanguageElements.LE_FieldID = 1) AND 
 					  (dbo.tblKartrisLanguageElements.LE_Value IS NOT NULL)
-GO
-EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
-Begin DesignProperties = 
-   Begin PaneConfigurations = 
-	  Begin PaneConfiguration = 0
-		 NumPanes = 4
-		 Configuration = "(H (1[41] 4[26] 2[14] 3) )"
-	  End
-	  Begin PaneConfiguration = 1
-		 NumPanes = 3
-		 Configuration = "(H (1 [50] 4 [25] 3))"
-	  End
-	  Begin PaneConfiguration = 2
-		 NumPanes = 3
-		 Configuration = "(H (1 [50] 2 [25] 3))"
-	  End
-	  Begin PaneConfiguration = 3
-		 NumPanes = 3
-		 Configuration = "(H (4 [30] 2 [40] 3))"
-	  End
-	  Begin PaneConfiguration = 4
-		 NumPanes = 2
-		 Configuration = "(H (1 [56] 3))"
-	  End
-	  Begin PaneConfiguration = 5
-		 NumPanes = 2
-		 Configuration = "(H (2 [66] 3))"
-	  End
-	  Begin PaneConfiguration = 6
-		 NumPanes = 2
-		 Configuration = "(H (4 [50] 3))"
-	  End
-	  Begin PaneConfiguration = 7
-		 NumPanes = 1
-		 Configuration = "(V (3))"
-	  End
-	  Begin PaneConfiguration = 8
-		 NumPanes = 3
-		 Configuration = "(H (1[56] 4[18] 2) )"
-	  End
-	  Begin PaneConfiguration = 9
-		 NumPanes = 2
-		 Configuration = "(H (1 [75] 4))"
-	  End
-	  Begin PaneConfiguration = 10
-		 NumPanes = 2
-		 Configuration = "(H (1[66] 2) )"
-	  End
-	  Begin PaneConfiguration = 11
-		 NumPanes = 2
-		 Configuration = "(H (4 [60] 2))"
-	  End
-	  Begin PaneConfiguration = 12
-		 NumPanes = 1
-		 Configuration = "(H (1) )"
-	  End
-	  Begin PaneConfiguration = 13
-		 NumPanes = 1
-		 Configuration = "(V (4))"
-	  End
-	  Begin PaneConfiguration = 14
-		 NumPanes = 1
-		 Configuration = "(V (2))"
-	  End
-	  ActivePaneConfig = 0
-   End
-   Begin DiagramPane = 
-	  Begin Origin = 
-		 Top = 0
-		 Left = 0
-	  End
-	  Begin Tables = 
-		 Begin Table = "tblKartrisLanguageElements"
-			Begin Extent = 
-			   Top = 6
-			   Left = 38
-			   Bottom = 149
-			   Right = 202
-			End
-			DisplayFlags = 280
-			TopColumn = 0
-		 End
-		 Begin Table = "tblKartrisPromotions"
-			Begin Extent = 
-			   Top = 6
-			   Left = 240
-			   Bottom = 247
-			   Right = 430
-			End
-			DisplayFlags = 280
-			TopColumn = 0
-		 End
-	  End
-   End
-   Begin SQLPane = 
-   End
-   Begin DataPane = 
-	  Begin ParameterDefaults = ""
-	  End
-	  Begin ColumnWidths = 15
-		 Width = 284
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-		 Width = 1500
-	  End
-   End
-   Begin CriteriaPane = 
-	  Begin ColumnWidths = 11
-		 Column = 4140
-		 Alias = 900
-		 Table = 1170
-		 Output = 720
-		 Append = 1400
-		 NewValue = 1170
-		 SortType = 1350
-		 SortOrder = 1410
-		 GroupBy = 1350
-		 Filter = 1350
-		 Or = 1350
-		 Or = 1350
-		 Or = 1350
-	  End
-   End
-End
-' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'vKartrisTypePromotions'
-GO
-EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'vKartrisTypePromotions'
+
 GO
 /****** Object:  UserDefinedFunction [dbo].[fnKartrisLanguageElement_GetItemValue]    Script Date: 01/23/2013 21:59:11 ******/
 SET ANSI_NULLS ON
@@ -16359,6 +16222,7 @@ CREATE FUNCTION [dbo].[fnKartrisLanguageElement_GetItemValue]
 	@LE_ParentID as int
 )
 RETURNS nvarchar(MAX)
+WITH SCHEMABINDING
 AS
 BEGIN
 	-- Declare the return variable here
@@ -16378,6 +16242,7 @@ BEGIN
 
 END
 GO
+
 /****** Object:  StoredProcedure [dbo].[_spKartrisPages_Delete]    Script Date: 01/23/2013 21:59:10 ******/
 SET ANSI_NULLS ON
 GO
@@ -22784,6 +22649,48 @@ BEGIN
 
 END
 GO
+
+
+/****** Object:  View [dbo].[vKartrisTypeProductsLite]    Script Date: 07/04/2019 15:58:12 ******/
+-- New view, this only includes P_Name. This way we don't link
+-- to the Language Elements table multiple times, and can therefore
+-- index this view.
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[vKartrisTypeProductsLite] WITH SCHEMABINDING
+AS
+SELECT     dbo.tblKartrisProducts.P_ID, dbo.tblKartrisLanguages.LANG_ID, CAST(dbo.tblKartrisLanguageElements.LE_Value AS NVARCHAR(255)) AS P_Name, 
+					  dbo.tblKartrisProducts.P_Live, dbo.tblKartrisProducts.P_Featured, 
+					  dbo.tblKartrisProducts.P_OrderVersionsBy, dbo.tblKartrisProducts.P_VersionsSortDirection, dbo.tblKartrisProducts.P_VersionDisplayType, 
+					  dbo.tblKartrisProducts.P_Reviews, dbo.tblKartrisProducts.P_SupplierID, dbo.tblKartrisProducts.P_Type, dbo.tblKartrisProducts.P_CustomerGroupID, 
+					  dbo.tblKartrisProducts.P_AverageRating, dbo.tblKartrisProducts.P_DateCreated, dbo.tblKartrisProducts.P_LastModified
+FROM         dbo.tblKartrisLanguageElements INNER JOIN
+					  dbo.tblKartrisLanguages ON dbo.tblKartrisLanguageElements.LE_LanguageID = dbo.tblKartrisLanguages.LANG_ID INNER JOIN
+					  dbo.tblKartrisProducts ON dbo.tblKartrisLanguageElements.LE_ParentID = dbo.tblKartrisProducts.P_ID
+WHERE     (dbo.tblKartrisLanguageElements.LE_FieldID = 1) AND 
+					  (NOT (dbo.tblKartrisLanguageElements.LE_Value IS NULL)) AND
+					  (dbo.tblKartrisLanguageElements.LE_TypeID = 2) 
+GO
+
+-- Add indexes to this view. Removing the other language element fields
+-- so we only have name means we can index it, should be much faster
+CREATE UNIQUE CLUSTERED INDEX P_LANG_ID
+ON [vKartrisTypeProductsLite] (P_ID, LANG_ID);
+
+CREATE NONCLUSTERED INDEX LANG_ID
+ON [vKartrisTypeProductsLite] (LANG_ID);
+
+CREATE NONCLUSTERED INDEX P_Name
+ON [vKartrisTypeProductsLite] (P_Name);
+
+CREATE NONCLUSTERED INDEX P_Live
+ON [vKartrisTypeProductsLite] (P_Live);
+GO
+
 /****** Object:  StoredProcedure [dbo].[_spKartrisProducts_GetRowsBetweenByCatID]    Script Date: 01/23/2013 21:59:10 ******/
 SET ANSI_NULLS ON
 GO
